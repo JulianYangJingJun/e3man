7fe1116b3934c9d58628b74c16bd7440c1304644
JulianYangJingJun <aqkt_436@163.com>
2019-05-29 09:56:48 +0800

    ggc

---
+++ E3ServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+namespace e3man\E3ServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+
+class E3ServiceProvider extends ServiceProvider
+{
+    /**
+     * 服务提供者加是否延迟加载.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([        
+            __DIR__.'/config/E3.php' => config_path('E3.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('e3man', function ($app) {
+            return new Packagetest($app['session'], $app['config']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['packagetest'];
+    }
+}
