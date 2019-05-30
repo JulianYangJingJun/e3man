13d5e0f79c944ad70ba93e09602096207f34fa05
JulianYangJingJun <aqkt_436@163.com>
2019-05-30 17:38:45 +0800

    ggd

--- src/E3.php
+++ src/E3.php
@@ -8,11 +8,6 @@ use GuzzleHttp\Client;
  * 
  */
 
-// define('E3_QYERY_URL', "http://121.41.163.189/e3test/webopm/web/?app_act=api/ec&app_mode=func");
-// define('E3_APP_KEY', "BAISON_E3_BSWMS");
-// define('E3_APP_SECRET', "770e253089a257a8070f984d5505aee2");
-// define('E3_WARE_HOUSE', " 11003005");
-
 class E3
 {
     /**
